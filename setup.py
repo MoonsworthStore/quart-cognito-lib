755b064e9424410b09b3aedae2fa8ea69e611d94
Connor Lewis <connor@moonsworth.com>
2023-11-27 20:19:45 +0000

    chore: expose services package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from os import path
 
 pwd = lambda f: path.join(path.abspath(path.dirname(__file__)), f)
@@ -10,5 +10,8 @@ setup(
     author_email='18327836+mblackgeo@users.noreply.github.com',
     url='https://github.com/mblackgeo/flask-cognito-lib',
     version='1.6.1',
-    packages=find_packages(),
+    packages=[
+        'quart_cognito_lib',
+        'quart_cognito_lib.services'
+    ]
 )
